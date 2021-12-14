ad8763de3cd260c8c0063aca2a108cc640538e8d
DefyCab <love.bellinder@gmail.com>
2021-12-14 22:48:21 +0100

    reduced fixture to 10 questions adds spec for GET request updated cypress.json create module Question and network upgrade to nodejs lts version 16.13.1

--- cypress/integration/userCanChoseQuizSpecs.js
+++ cypress/integration/userCanChoseQuizSpecs.js
@@ -1,24 +1,32 @@
-describe("Displays Quiz options");
-before(() => {
-  cy.intercept("PUTS", "https://foo-bar**", {
-    fixture: "historyHardQuestion.json",
-  });
-  cy.visit("/");
-  cy.get("[data-cy=number-of-questions]").type(50);
-  cy.get("[data-cy=topic]").click().get("[data-cy=History]").click();
-  // click dropdown => search topic "history" => click
-  cy.get("[data-cy=difficulty]").click().get("[data-cy=Hard]").click();
-  // placeholders names. Drop-down tables.
-});
+describe('Displays Quiz options', () => {
+  before(() => {
+    cy.intercept('GET', '**/api/questions', {
+      fixture: './historyHardQuestions.json',
+    }).as('Questions.index')
+    cy.visit('/')
+  })
+  it('is expected to make a network call with a status 200', () => {
+    cy.wait('@Questions.index').its('response.statusCode').should('eq', 200)
+  })
 
-it("is expected to see input field", () => {
-  cy.get("[data-cy=number-of-questions]");
-});
+  it('is expected to generate an array of 10 set of questions', () => {
+    cy.get('[data-cy=question-array]').children().should('have.length', 10)
+  })
+})
 
-it("is expected to see topic drop down", () => {
-  cy.get("[data-cy=topic]");
-});
+// cy.get("[data-cy=topic]").click().get("[data-cy=History]").click();
+// // click dropdown => search topic "history" => click
+// cy.get("[data-cy=difficulty]").click().get("[data-cy=Hard]").click();
+// // placeholders names. Drop-down tables.
+// });
 
-it("is expected to see difficulty drop down", () => {
-  cy.get("[data-cy=difficulty]");
-});
+// it("is expected to see input field", () => {
+//   cy.get("[data-cy=number-of-questions]");
+// });
+
+// it("is expected to see topic drop down", () => {
+//   cy.get("[data-cy=topic]");
+// });
+
+// it("is expected to see difficulty drop down", () => {
+//   cy.get("[data-cy=difficulty]");
---
+++ src/modules/Questions.js
@@ -0,0 +1,10 @@
+import { api } from "./network"
+
+const Questions = {
+  async index() {
+    const { data } = await api.get("/questions")
+    return data.questions
+  }
+}
+
+export default Questions
